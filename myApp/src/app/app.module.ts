613ab7bc658016fcacbb26565bc06858b690f941
Carmine <Carmine.Tramaglino@nntdata.com>
2018-10-12 18:00:42 +0200

    internationalization

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 import {AppComponent} from './app.component';
@@ -12,6 +12,13 @@ import {MessagesComponent} from './components/messages/messages.component';
 import {AppRoutingModule} from './/app-routing.module';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import { FilmSearchComponent } from './components/film-search/film-search.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {NgxPaginationModule} from 'ngx-pagination';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient,  './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -26,8 +33,16 @@ import { FilmSearchComponent } from './components/film-search/film-search.compon
     BrowserModule,
     NgbModule,
     FormsModule,
+    NgxPaginationModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
